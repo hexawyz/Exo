0b045bf3c445ca4d33c65ffa2ca22d5a527ac4f2
GoldenCrystal <8518235+GoldenCrystal@users.noreply.github.com>
2021-07-04 02:54:04 +0200

    🖨 Add a private copy of System.ServiceProcess.

---
+++ Exo.ServiceProcess/ReadMe.md
@@ -0,0 +1,4 @@
+﻿# Exo.ServiceProcess
+
+This is an internal version of System.ServiceProcess for the purposes of this project.
+Most code is © Microsoft and .NET Contributors, and some modifications will be added in order to expose required features such as device notifications.
---
+++ Exo.ServiceProcess/System/ServiceProcess/SessionChangeDescription.cs
@@ -0,0 +1,50 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace System.ServiceProcess
+{
+    public readonly struct SessionChangeDescription
+    {
+        internal SessionChangeDescription(SessionChangeReason reason, int id)
+        {
+            Reason = reason;
+            SessionId = id;
+        }
+
+        public SessionChangeReason Reason { get; }
+
+        public int SessionId { get; }
+
+        public override bool Equals([NotNullWhen(true)] object? obj)
+        {
+            if (!(obj is SessionChangeDescription))
+            {
+                return false;
+            }
+
+            return Equals((SessionChangeDescription)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (int)Reason ^ SessionId;
+        }
+
+        public bool Equals(SessionChangeDescription changeDescription)
+        {
+            return (Reason == changeDescription.Reason) && (SessionId == changeDescription.SessionId);
+        }
+
+        public static bool operator ==(SessionChangeDescription a, SessionChangeDescription b)
+        {
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(SessionChangeDescription a, SessionChangeDescription b)
+        {
+            return !a.Equals(b);
+        }
+    }
+}
