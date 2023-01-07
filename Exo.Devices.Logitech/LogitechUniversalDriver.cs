a9747c5a42d8b51a1a8300d4f0e155cbc241a9d4
GoldenCrystal <8518235+GoldenCrystal@users.noreply.github.com>
2023-01-07 21:18:19 +0100

    ✏️ Start working on Logitech keyboard details.

---
+++ Exo.Devices.Logitech/LogitechUniversalDriver.cs
@@ -0,0 +1,20 @@
+using DeviceTools.HumanInterfaceDevices;
+using Exo.Core;
+using Exo.Core.Features;
+
+namespace Exo.Devices.Logitech;
+
+[DeviceId(VendorIdSource.Usb, 0x046D, 0xB361)]
+[DeviceId(VendorIdSource.Usb, 0x046D, 0xB36A)]
+public class LogitechUniversalDriver : Driver, IDeviceDriver<IKeyboardDeviceFeature>
+{
+	public override IDeviceFeatureCollection<IDeviceFeature> Features => throw new NotImplementedException();
+
+	IDeviceFeatureCollection<IKeyboardDeviceFeature> IDeviceDriver<IKeyboardDeviceFeature>.Features { get; }
+
+	public override string FriendlyName { get; }
+
+	protected override string GetConfigurationKey() => throw new NotImplementedException();
+
+	public static ValueTask<LogitechUniversalDriver> CreateAsync(string path) => throw new NotImplementedException();
+}
