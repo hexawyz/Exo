662cdc5c6fac3cfbfa68e7688b3bd4621e192b03
GoldenCrystal <8518235+GoldenCrystal@users.noreply.github.com>
2023-07-14 23:51:25 +0200

    ♻️ Refactor LG monitor driver into a separate transport class.
    May want to split this further afterwards, but let's see once more features are implemented.

--- Exo.Devices.Lg.Monitors/LgMonitorDriver.cs
+++ Exo.Devices.Lg.Monitors/LgMonitorDriver.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Concurrent;
 using System.Collections.Immutable;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -98,76 +100,27 @@ public class LgMonitorDriver : HidDriver, IDeviceDriver<IMonitorDeviceFeature>
 		}
 
 		byte sessionId = (byte)Random.Shared.Next(1, 256);
-		var buffer = GC.AllocateArray<byte>(65, true);
-		var hidStream = new HidFullDuplexStream(deviceInterfaceName);
-		try
-		{
-			await GetDeviceInformationAsync(hidStream, sessionId, buffer, cancellationToken).ConfigureAwait(false);
-			return new LgMonitorDriver
-			(
-				new HidFullDuplexStream(deviceInterfaceName),
-				Unsafe.As<string[], ImmutableArray<string>>(ref deviceNames),
-				friendlyName
-			);
-		}
-		catch
-		{
-			await hidStream.DisposeAsync().ConfigureAwait(false);
-			throw;
-		}
+		var transport = await HidI2cTransport.CreateAsync(new HidFullDuplexStream(deviceInterfaceName), sessionId, HidI2cTransport.DefaultDeviceAddress, cancellationToken).ConfigureAwait(false);
+		return new LgMonitorDriver
+		(
+			transport,
+			Unsafe.As<string[], ImmutableArray<string>>(ref deviceNames),
+			friendlyName
+		);
 	}
 
-	private static async Task GetDeviceInformationAsync(HidFullDuplexStream hidStream, byte sessionId, byte[] buffer, CancellationToken cancellationToken)
-	{
-		Array.Clear(buffer);
-
-		buffer[1] = 0x0C;
-		buffer[3] = sessionId;
-		buffer[4] = 0x01;
-		buffer[5] = 0x80;
-		buffer[6] = 0x1a;
-		buffer[7] = 0x06;
-
-		var memory = MemoryMarshal.CreateFromPinnedArray(buffer, 0, buffer.Length);
-
-		await hidStream.WriteAsync(memory, cancellationToken).ConfigureAwait(false);
-
-		Array.Clear(buffer);
-
-		while (true)
-		{
-			int count = await hidStream.ReadAsync(memory, cancellationToken).ConfigureAwait(false);
-			if (count != buffer.Length)
-			{
-				throw new EndOfStreamException("The connection to the device has ended abruptly.");
-			}
-
-			if (buffer[1] == 0x0c && buffer[2] == 0x00 && buffer[3] == sessionId && buffer[4] == 0x00)
-			{
-				if (buffer.AsSpan(10, 3).SequenceEqual("HID"u8))
-				{
-					break;
-				}
-				else
-				{
-					throw new InvalidDataException("The device handshake failed because invalid data was returned.");
-				}
-			}
-		}
-	}
-
-	private readonly HidFullDuplexStream _hidFullDuplexStream;
+	private readonly HidI2cTransport _transport;
 	private readonly IDeviceFeatureCollection<IDeviceFeature> _allFeatures;
 	private readonly IDeviceFeatureCollection<IMonitorDeviceFeature> _monitorFeatures;
 
 	private LgMonitorDriver
 	(
-		HidFullDuplexStream hidFullDuplexStream,
+		HidI2cTransport transport,
 		ImmutableArray<string> deviceNames,
 		string friendlyName
 	) : base(deviceNames, friendlyName, default)
 	{
-		_hidFullDuplexStream = hidFullDuplexStream;
+		_transport = transport;
 		_monitorFeatures = FeatureCollection.Empty<IMonitorDeviceFeature>();
 		_allFeatures = FeatureCollection.Empty<IDeviceFeature>();
 	}
@@ -176,4 +129,215 @@ public class LgMonitorDriver : HidDriver, IDeviceDriver<IMonitorDeviceFeature>
 	public override IDeviceFeatureCollection<IDeviceFeature> Features => _allFeatures;
 
 	public override ValueTask DisposeAsync() => ValueTask.CompletedTask;
+}
+
+public sealed class HidI2cTransport : IAsyncDisposable
+{
+	public const int DefaultDeviceAddress = 0x37;
+
+	// The message length is hardcoded to 64 bytes + report ID.
+	private const int MessageLength = 65;
+
+	private const int WriteStateReady = 0;
+	private const int WriteStateReserved = 1;
+	private const int WriteStateDisposed = -1;
+
+	private abstract class ResponseWaitState
+	{
+		public abstract void Complete(ReadOnlySpan<byte> message);
+	}
+
+	private abstract class ResponseWaitState<T> : ResponseWaitState
+	{
+		public TaskCompletionSource<T> TaskCompletionSource { get; } = new();
+	}
+
+	private sealed class HandshakeResponseWaitState : ResponseWaitState<bool>
+	{
+		public override void Complete(ReadOnlySpan<byte> message)
+		{
+			if (message[0] == 0x0c && message[3] == 0x00)
+			{
+				if (message.Slice(9, 3).SequenceEqual("HID"u8))
+				{
+					TaskCompletionSource.TrySetResult(true);
+				}
+				else
+				{
+					TaskCompletionSource.TrySetResult(false);
+				}
+			}
+		}
+	}
+
+	private readonly HidFullDuplexStream _stream;
+	private readonly byte[] _buffers;
+	private readonly byte _sessionId;
+	private readonly byte _deviceAddress;
+	private readonly CancellationTokenSource _cancellationTokenSource;
+	private readonly Task _readAsyncTask;
+	private readonly ConcurrentDictionary<byte, ResponseWaitState> _pendingOperations;
+	private int _writeState;
+
+	/// <summary>Creates a new instance of the class <see cref="HidI2cTransport"/>.</summary>
+	/// <param name="stream">A stream to use for receiving and sending messages.</param>
+	/// <param name="sessionId">A byte value to be used for identifying requests done by the instance.</param>
+	/// <param name="cancellationToken"></param>
+	public static Task<HidI2cTransport> CreateAsync(HidFullDuplexStream stream, byte sessionId, CancellationToken cancellationToken)
+		=> CreateAsync(stream, sessionId, DefaultDeviceAddress, cancellationToken);
+
+	/// <summary>Creates a new instance of the class <see cref="HidI2cTransport"/>.</summary>
+	/// <param name="stream">A stream to use for receiving and sending messages.</param>
+	/// <param name="sessionId">A byte value to be used for identifying requests done by the instance.</param>
+	/// <param name="deviceAddress">The address of the I2C device. Defaults to <c>0x37</c>.</param>
+	/// <param name="cancellationToken"></param>
+	public static async Task<HidI2cTransport> CreateAsync(HidFullDuplexStream stream, byte sessionId, byte deviceAddress, CancellationToken cancellationToken)
+	{
+		var transport = new HidI2cTransport(stream, sessionId, deviceAddress);
+		try
+		{
+			await transport.HandshakeAsync(cancellationToken).ConfigureAwait(false);
+		}
+		catch
+		{
+			await transport.DisposeAsync().ConfigureAwait(false);
+			throw;
+		}
+		return transport;
+	}
+
+	public async ValueTask DisposeAsync()
+	{
+		_cancellationTokenSource.Cancel();
+		await _stream.DisposeAsync().ConfigureAwait(false);
+		await _readAsyncTask.ConfigureAwait(false);
+	}
+
+	private HidI2cTransport(HidFullDuplexStream stream, byte sessionId, byte deviceAddress)
+	{
+		_stream = stream;
+		_sessionId = sessionId;
+		_deviceAddress = deviceAddress;
+		_buffers = GC.AllocateUninitializedArray<byte>(2 * MessageLength, true);
+		_buffers[65] = 0; // Zero-initialize the write report ID.
+		_cancellationTokenSource = new CancellationTokenSource();
+		_readAsyncTask = ReadAsync(_cancellationTokenSource.Token);
+		_pendingOperations = new();
+	}
+
+	private byte BeginWrite()
+	{
+		int oldState = Interlocked.CompareExchange(ref _writeState, WriteStateReserved, WriteStateReady);
+		if (oldState != WriteStateReady)
+		{
+			if (oldState == WriteStateDisposed) throw new ObjectDisposedException(nameof(HidI2cTransport));
+			else throw new InvalidOperationException("A write operation is already pending.");
+		}
+		return (byte)(oldState >>> 24);
+	}
+
+	private void EndWrite(byte sequenceNumber)
+	{
+		Interlocked.CompareExchange(ref _writeState, (sequenceNumber + 1) << 24 | WriteStateReady, sequenceNumber << 24 | WriteStateReserved);
+	}
+
+	private async Task HandshakeAsync(CancellationToken cancellationToken)
+	{
+		bool result;
+		byte sequenceNumber = BeginWrite();
+		try
+		{
+			var buffer = MemoryMarshal.CreateFromPinnedArray(_buffers, MessageLength, MessageLength);
+
+			WriteHandshakeRequest(buffer.Span[1..], _sessionId, 0);
+
+			var waitState = new HandshakeResponseWaitState();
+
+			_pendingOperations.TryAdd(sequenceNumber, waitState);
+
+			try
+			{
+				await _stream.WriteAsync(buffer, cancellationToken).ConfigureAwait(false);
+				result = await waitState.TaskCompletionSource.Task.WaitAsync(cancellationToken).ConfigureAwait(false);
+			}
+			catch (OperationCanceledException)
+			{
+				_pendingOperations.TryRemove(new(sequenceNumber, waitState));
+				throw;
+			}
+		}
+		finally
+		{
+			EndWrite(sequenceNumber);
+		}
+
+		if (!result)
+		{
+			throw new InvalidDataException("The device handshake failed because invalid data was returned.");
+		}
+	}
+
+	private static void WriteHandshakeRequest(Span<byte> buffer, byte sessionId, byte sequenceNumber)
+	{
+		buffer.Clear();
+
+		buffer[0] = 0x0C;
+		buffer[1] = sequenceNumber;
+		buffer[2] = sessionId;
+		buffer[3] = 0x01;
+		buffer[4] = 0x80;
+		buffer[5] = 0x1a;
+		buffer[6] = 0x06;
+	}
+
+	private async Task ReadAsync(CancellationToken cancellationToken)
+	{
+		var buffer = MemoryMarshal.CreateFromPinnedArray(_buffers, 0, 65);
+		try
+		{
+			while (true)
+			{
+				// Data is received in fixed length packets, so we expect to always receive exactly the number of bytes that the buffer can hold.
+				var remaining = buffer;
+				do
+				{
+					int count;
+					try
+					{
+						count = await _stream.ReadAsync(remaining, cancellationToken).ConfigureAwait(false);
+					}
+					catch (OperationCanceledException)
+					{
+						return;
+					}
+
+					if (count == 0)
+					{
+						return;
+					}
+
+					remaining = remaining.Slice(count);
+				}
+				while (remaining.Length != 0);
+
+				ProcessReadMessage(buffer.Span[1..]);
+			}
+		}
+		catch
+		{
+			// TODO: Log the exception
+		}
+	}
+
+	private void ProcessReadMessage(ReadOnlySpan<byte> message)
+	{
+		if (message[2] != _sessionId) return;
+
+		byte sequenceNumber = message[1];
+
+		if (_pendingOperations.TryRemove(sequenceNumber, out var state))
+		{
+			state.Complete(message);
+		}
+	}
 }
