abd1c6524c837205949f2842fdca5a5301a48d2a
hexawyz <8518235+hexawyz@users.noreply.github.com>
2024-04-15 21:23:24 +0200

    🎨 Improve the Sensors UI with better automatic scales and accent colors.

--- Exo.Settings.Ui/HistoryDataToPathGeometryConverter.cs
+++ Exo.Settings.Ui/HistoryDataToPathGeometryConverter.cs
@@ -7,9 +7,9 @@ namespace Exo.Settings.Ui;
 internal sealed class HistoryDataToPathGeometryConverter : IValueConverter
 {
 	public object? Convert(object value, Type targetType, object parameter, string language)
-		=> value is LiveSensorDetailsViewModel.HistoryData history ? Convert(history) : null;
+		=> value is LiveSensorDetailsViewModel.HistoryData history ? Convert(history, parameter is not null && System.Convert.ToBoolean(parameter)) : null;
 
-	private PathGeometry Convert(LiveSensorDetailsViewModel.HistoryData history)
+	private PathGeometry Convert(LiveSensorDetailsViewModel.HistoryData history, bool closeShape)
 	{
 		// NB: This is very rough and WIP.
 		// It should probably be ported to a dedicated chart drawing component afterwards.
@@ -24,9 +24,6 @@ internal sealed class HistoryDataToPathGeometryConverter : IValueConverter
 			maxValue = Math.Max(value, maxValue);
 		}
 
-		double scaleMin = Math.Round(minValue, 0, MidpointRounding.ToNegativeInfinity);
-		double scaleMax = Math.Round(maxValue, 0, MidpointRounding.ToPositiveInfinity);
-
 		// Anchor the scale to zero if necessary.
 		if (maxValue < 0) maxValue = 0;
 		if (minValue > 0) minValue = 0;
@@ -34,26 +31,98 @@ internal sealed class HistoryDataToPathGeometryConverter : IValueConverter
 		// Force the chart to not be fully empty if the min and max are both zero. (result of previous adjustments)
 		if (minValue == maxValue) maxValue = 1;
 
+		var (scaleMin, scaleMax, _) = NiceScale.Compute(minValue, maxValue);
+
 		double scaleAmplitudeX = history.Length - 1;
 		double scaleAmplitudeY = maxValue - minValue;
-		double outputAmplitudeX = 100;
-		double outputAmplitudeY = 100;
+		double outputAmplitudeX = 99;
+		double outputAmplitudeY = 99;
 
 		var figure = new PathFigure();
 
-		double firstValue = history[0];
-
-		figure.StartPoint = new(0, outputAmplitudeY - (firstValue - minValue) * outputAmplitudeY / scaleAmplitudeY);
-		for (int i = 1; i < history.Length; i++)
+		double firstValue;
+		double firstValueY;
+		int j;
+		if (closeShape)
 		{
-			double value = history[i];
-			double x = i * outputAmplitudeX / scaleAmplitudeX;
+			firstValue = 0;
+			j = 0;
+		}
+		else
+		{
+			firstValue = history[0];
+			j = 1;
+		}
+
+		figure.StartPoint = new(0.5, 0.5f + (firstValueY = outputAmplitudeY - (firstValue - minValue) * outputAmplitudeY / scaleAmplitudeY));
+		for (; j < history.Length; j++)
+		{
+			double value = history[j];
+			double x = j * outputAmplitudeX / scaleAmplitudeX;
 			double y = outputAmplitudeY - (value - minValue) * outputAmplitudeY / scaleAmplitudeY;
-			figure.Segments.Add(new LineSegment() { Point = new(x, y) });
+			figure.Segments.Add(new LineSegment() { Point = new(x + 0.5, y + 0.5) });
+		}
+
+		if (closeShape)
+		{
+			figure.Segments.Add(new LineSegment() { Point = new(outputAmplitudeX + 0.5, firstValueY + 0.5) });
 		}
 
 		return new PathGeometry() { Figures = { figure } };
 	}
 
 	public object ConvertBack(object value, Type targetType, object parameter, string language) => throw new NotSupportedException();
+}
+
+// See: https://stackoverflow.com/a/16363437
+internal static class NiceScale
+{
+	public static (double Min, double Max, double TickSpacing) Compute(double min, double max, double tickCount = 10)
+	{
+		double range = MakeNice(max - min, false);
+		double tickSpacing = MakeNice(range / (tickCount - 1), true);
+		double niceMin = min < 0 ?
+			Math.Ceiling(min / tickSpacing) * tickSpacing :
+			Math.Floor(min / tickSpacing) * tickSpacing;
+		double niceMax = min < 0 ?
+			Math.Floor(max / tickSpacing) * tickSpacing :
+			Math.Ceiling(max / tickSpacing) * tickSpacing;
+
+		return (niceMin, niceMax, tickSpacing);
+	}
+
+	public static double MakeNice(double number, bool round)
+	{
+		double exponent;
+		double fraction;
+		double niceFraction;
+
+		exponent = Math.Floor(Math.Log10(number));
+		fraction = number / Math.Pow(10, exponent);
+
+		if (round)
+		{
+			if (fraction < 1.5)
+				niceFraction = 1;
+			else if (fraction < 3)
+				niceFraction = 2;
+			else if (fraction < 7)
+				niceFraction = 5;
+			else
+				niceFraction = 10;
+		}
+		else
+		{
+			if (fraction <= 1)
+				niceFraction = 1;
+			else if (fraction <= 2)
+				niceFraction = 2;
+			else if (fraction <= 5)
+				niceFraction = 5;
+			else
+				niceFraction = 10;
+		}
+
+		return niceFraction * Math.Pow(10, exponent);
+	}
 }
