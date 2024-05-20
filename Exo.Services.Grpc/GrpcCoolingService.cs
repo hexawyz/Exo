676f6df56b2bea63dc3431c27dfbc6b45a13ec59
hexawyz <8518235+hexawyz@users.noreply.github.com>
2024-05-21 00:32:17 +0200

    ✨ Implement the GRPC code for setting cooling modes.

--- Exo.Services.Grpc/GrpcCoolingService.cs
+++ Exo.Services.Grpc/GrpcCoolingService.cs
@@ -1,6 +1,8 @@
+using System.Collections.Immutable;
 using System.Runtime.CompilerServices;
 using Exo.Contracts.Ui.Settings;
 using Exo.Contracts.Ui.Settings.Cooling;
+using Exo.Cooling;
 using Microsoft.Extensions.Logging;
 
 namespace Exo.Service.Grpc;
@@ -37,7 +39,84 @@ internal sealed class GrpcCoolingService : ICoolingService
 		yield break;
 	}
 
-	public ValueTask SetAutomaticCoolingAsync(AutomaticCoolingParameters parameters, CancellationToken cancellationToken) => throw new NotImplementedException();
-	public ValueTask SetFixedCoolingAsync(FixedCoolingParameters parameters, CancellationToken cancellationToken) => throw new NotImplementedException();
-	public ValueTask SetSoftwareControlCurveCoolingAsync(SoftwareCurveCoolingParameters parameters, CancellationToken cancellationToken) => throw new NotImplementedException();
+	public ValueTask SetAutomaticCoolingAsync(AutomaticCoolingParameters parameters, CancellationToken cancellationToken)
+		=> _coolingService.SetAutomaticPowerAsync(parameters.DeviceId, parameters.CoolerId, cancellationToken);
+
+	public ValueTask SetFixedCoolingAsync(FixedCoolingParameters parameters, CancellationToken cancellationToken)
+		=> _coolingService.SetFixedPowerAsync(parameters.DeviceId, parameters.CoolerId, parameters.Power, cancellationToken);
+
+	public ValueTask SetSoftwareControlCurveCoolingAsync(SoftwareCurveCoolingParameters parameters, CancellationToken cancellationToken)
+	{
+		if (parameters.FallbackValue > 100) throw new ArgumentException("The fallback value must be less than or equal to 100%.");
+
+		return parameters.ControlCurve.RawValue switch
+		{
+			UnsignedIntegerCoolingControlCurve unsignedIntegerCurve =>
+				_coolingService.SetSoftwareControlCurveAsync
+				(
+					parameters.CoolingDeviceId,
+					parameters.CoolerId,
+					parameters.SensorDeviceId,
+					parameters.SensorId,
+					parameters.FallbackValue,
+					new InterpolatedSegmentControlCurve<ulong, byte>
+					(
+						ImmutableArray.CreateRange(unsignedIntegerCurve.SegmentPoints, dp => new DataPoint<ulong, byte>(dp.X, checked((byte)dp.Y))),
+						unsignedIntegerCurve.InitialValue,
+						MonotonicityValidators<byte>.StrictlyIncreasingUpTo100
+					),
+					cancellationToken
+				),
+			SignedIntegerCoolingControlCurve signedIntegerCurve =>
+				_coolingService.SetSoftwareControlCurveAsync
+				(
+					parameters.CoolingDeviceId,
+					parameters.CoolerId,
+					parameters.SensorDeviceId,
+					parameters.SensorId,
+					parameters.FallbackValue,
+					new InterpolatedSegmentControlCurve<long, byte>
+					(
+						ImmutableArray.CreateRange(signedIntegerCurve.SegmentPoints, dp => new DataPoint<long, byte>(dp.X, checked((byte)dp.Y))),
+						signedIntegerCurve.InitialValue,
+						MonotonicityValidators<byte>.StrictlyIncreasingUpTo100
+					),
+					cancellationToken
+				),
+			SinglePrecisionFloatingPointCoolingControlCurve singleFloatCurve =>
+				_coolingService.SetSoftwareControlCurveAsync
+				(
+					parameters.CoolingDeviceId,
+					parameters.CoolerId,
+					parameters.SensorDeviceId,
+					parameters.SensorId,
+					parameters.FallbackValue,
+					new InterpolatedSegmentControlCurve<float, byte>
+					(
+						ImmutableArray.CreateRange(singleFloatCurve.SegmentPoints, dp => new DataPoint<float, byte>(dp.X, checked((byte)dp.Y))),
+						singleFloatCurve.InitialValue,
+						MonotonicityValidators<byte>.StrictlyIncreasingUpTo100
+					),
+					cancellationToken
+				),
+			DoublePrecisionFloatingPointCoolingControlCurve doubleFloatCurve =>
+				_coolingService.SetSoftwareControlCurveAsync
+				(
+					parameters.CoolingDeviceId,
+					parameters.CoolerId,
+					parameters.SensorDeviceId,
+					parameters.SensorId,
+					parameters.FallbackValue,
+					new InterpolatedSegmentControlCurve<double, byte>
+					(
+						ImmutableArray.CreateRange(doubleFloatCurve.SegmentPoints, dp => new DataPoint<double, byte>(dp.X, checked((byte)dp.Y))),
+						doubleFloatCurve.InitialValue,
+						MonotonicityValidators<byte>.StrictlyIncreasingUpTo100
+					),
+					cancellationToken
+				),
+			null => throw new ArgumentException("Missing control curve."),
+			_ => throw new InvalidOperationException("Unsupported control curve data."),
+		};
+	}
 }
