3039d23772effb24062c9c2e7682a438567da580
hexawyz <8518235+hexawyz@users.noreply.github.com>
2024-07-09 23:00:36 +0200

    🎨 Monitor: Move performance settings into their own sub-section. (Declutter 4/N)

---
+++ Exo.Settings.Ui/MonitorPerformanceSettingsControl.xaml.cs
@@ -0,0 +1,8 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace Exo.Settings.Ui;
+
+internal sealed partial class MonitorPerformanceSettingsControl : UserControl
+{
+	public MonitorPerformanceSettingsControl() => InitializeComponent();
+}
