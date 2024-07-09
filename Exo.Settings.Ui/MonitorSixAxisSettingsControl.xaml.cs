be33c75ab0233981db57d1701ecc5ee0caff65eb
hexawyz <8518235+hexawyz@users.noreply.github.com>
2024-07-09 22:43:25 +0200

    🎨 Monitor: Move 6 Color settings into their own sub-section. (Declutter 2/N)

---
+++ Exo.Settings.Ui/MonitorSixAxisSettingsControl.xaml.cs
@@ -0,0 +1,8 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace Exo.Settings.Ui;
+
+internal sealed partial class MonitorSixAxisSettingsControl : UserControl
+{
+	public MonitorSixAxisSettingsControl() => InitializeComponent();
+}
