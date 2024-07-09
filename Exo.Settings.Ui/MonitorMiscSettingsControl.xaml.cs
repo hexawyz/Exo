146dd5a6309537cebe59195aff6ed98903d5b649
hexawyz <8518235+hexawyz@users.noreply.github.com>
2024-07-09 22:56:05 +0200

    🎨 Monitor: Move miscellaneous settings into their own sub-section. (Declutter 3/N)

---
+++ Exo.Settings.Ui/MonitorMiscSettingsControl.xaml.cs
@@ -0,0 +1,8 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace Exo.Settings.Ui;
+
+internal sealed partial class MonitorMiscSettingsControl : UserControl
+{
+	public MonitorMiscSettingsControl() => InitializeComponent();
+}
