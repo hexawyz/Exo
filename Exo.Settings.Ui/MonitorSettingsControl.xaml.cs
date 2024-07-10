a68be6aecb50d0864aa16c4ab32d9a9b0cf7dff1
hexawyz <8518235+hexawyz@users.noreply.github.com>
2024-07-10 22:23:04 +0200

    ✨ Automatically refresh monitor settings when displaying the monitor settings control.

--- Exo.Settings.Ui/MonitorSettingsControl.xaml.cs
+++ Exo.Settings.Ui/MonitorSettingsControl.xaml.cs
@@ -1,3 +1,5 @@
+using Exo.Settings.Ui.ViewModels;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
 namespace Exo.Settings.Ui;
@@ -7,5 +9,14 @@ internal sealed partial class MonitorSettingsControl : UserControl
 	public MonitorSettingsControl()
 	{
 		InitializeComponent();
+		DataContextChanged += OnDataContextChanged;
+	}
+
+	private void OnDataContextChanged(FrameworkElement sender, DataContextChangedEventArgs args)
+	{
+		if (args.NewValue is MonitorDeviceFeaturesViewModel vm)
+		{
+			vm.RefreshCommand.Execute(vm);
+		}
 	}
 }
