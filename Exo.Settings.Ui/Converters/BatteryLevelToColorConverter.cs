52f9c451dd0aea47d1d83fb08900bc076818c892
hexawyz <8518235+hexawyz@users.noreply.github.com>
2024-07-28 18:08:17 +0200

    🎨 Make the battery level gauges use a different color based on battery level.

---
+++ Exo.Settings.Ui/Converters/BatteryLevelToColorConverter.cs
@@ -0,0 +1,29 @@
+using Microsoft.UI;
+using Microsoft.UI.Xaml.Data;
+
+namespace Exo.Settings.Ui.Converters;
+
+internal sealed class BatteryLevelToColorConverter : IValueConverter
+{
+	private static readonly object Lime = Colors.Lime;
+	private static readonly object Orange = Colors.Orange;
+	private static readonly object Red = Colors.Red;
+
+	public object? Convert(object value, Type targetType, object parameter, string language)
+	{
+		if (value is float f)
+		{
+			if (f < 0.2f)
+			{
+				if (f < 0.1f)
+				{
+					return Red;
+				}
+				return Orange;
+			}
+		}
+		return Lime;
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, string language) => throw new NotSupportedException();
+}
