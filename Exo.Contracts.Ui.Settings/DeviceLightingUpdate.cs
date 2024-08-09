e959e12da7a54d81bc9dcccc8ac55dcb543ace53
hexawyz <8518235+hexawyz@users.noreply.github.com>
2024-08-09 21:51:46 +0200

    🐛 Fix problems with empty lighting updates.

--- Exo.Contracts.Ui.Settings/DeviceLightingUpdate.cs
+++ Exo.Contracts.Ui.Settings/DeviceLightingUpdate.cs
@@ -8,10 +8,18 @@ public sealed class DeviceLightingUpdate
 {
 	[DataMember(Order = 1)]
 	public required Guid DeviceId { get; init; }
+
 	[DataMember(Order = 2)]
 	public required bool ShouldPersist { get; init; }
+
 	[DataMember(Order = 3)]
 	public byte BrightnessLevel { get; init; }
+
+	private readonly ImmutableArray<ZoneLightEffect> _zoneEffects;
 	[DataMember(Order = 4)]
-	public required ImmutableArray<ZoneLightEffect> ZoneEffects { get; init; }
+	public required ImmutableArray<ZoneLightEffect> ZoneEffects
+	{
+		get => _zoneEffects.NotNull();
+		init => _zoneEffects = value.NotNull();
+	}
 }
--- Exo.Contracts.Ui.Settings/MultiDeviceLightingUpdates.cs
+++ Exo.Contracts.Ui.Settings/MultiDeviceLightingUpdates.cs
@@ -6,6 +6,11 @@ namespace Exo.Contracts.Ui.Settings;
 [DataContract]
 public sealed class MultiDeviceLightingUpdates
 {
+	private readonly ImmutableArray<DeviceLightingUpdate> _deviceUpdates;
 	[DataMember(Order = 1)]
-	public required ImmutableArray<DeviceLightingUpdate> DeviceUpdates { get; init; }
+	public required ImmutableArray<DeviceLightingUpdate> DeviceUpdates
+	{
+		get => _deviceUpdates.NotNull();
+		init => _deviceUpdates = value.NotNull();
+	}
 }
