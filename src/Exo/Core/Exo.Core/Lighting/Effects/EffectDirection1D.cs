8e6c78169a2461b550b939a525091c73dde97a21
hexawyz <8518235+hexawyz@users.noreply.github.com>
2024-09-28 01:49:35 +0200

    ✨ Add 1-directional wave lighting effect definition.

---
+++ src/Exo/Core/Exo.Core/Lighting/Effects/EffectDirection1D.cs
@@ -0,0 +1,7 @@
+﻿namespace Exo.Lighting.Effects;
+
+public enum EffectDirection1D
+{
+	Forward = 0,
+	Backwards = 1,
+}
---
+++ src/Exo/Core/Exo.Core/Lighting/Effects/SpectrumWaveEffect1D.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace Exo.Lighting.Effects;
+
+[DataContract]
+[TypeId(0xB3B1B3B2, 0x5616, 0x453C, 0xA3, 0x81, 0x3D, 0x87, 0xA3, 0x05, 0x15, 0xA0)]
+public readonly struct SpectrumWaveEffect1D : ILightingEffect
+{
+	[DataMember(Order = 1)]
+	[Display(Name = "Direction")]
+	public EffectDirection1D Direction { get; }
+
+	public SpectrumWaveEffect1D(EffectDirection1D direction)
+	{
+		Direction = direction;
+	}
+}
