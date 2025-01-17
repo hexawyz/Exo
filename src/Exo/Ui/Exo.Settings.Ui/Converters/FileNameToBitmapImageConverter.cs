1724cb9d27964558825f570344f7dc19bb609555
hexawyz <8518235+hexawyz@users.noreply.github.com>
2025-01-18 00:35:34 +0100

    ✨ Add a cache to the filename to BitmapImage converter.
    I noticed that the filename property would be read multiple times in succession on the same instance. This should at least make this scenario less heavy on resources. This can be improved later on to have some kind of minimum retention timer for images, in order to mitigate the potentially too efficient GC. (If needed)

--- src/Exo/Ui/Exo.Settings.Ui/Converters/FileNameToBitmapImageConverter.cs
+++ src/Exo/Ui/Exo.Settings.Ui/Converters/FileNameToBitmapImageConverter.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Media.Imaging;
 using Windows.Storage.Streams;
 
@@ -6,11 +6,26 @@ namespace Exo.Settings.Ui.Converters;
 
 internal sealed class FileNameToBitmapImageConverter : IValueConverter
 {
+	private readonly Dictionary<string, WeakReference<BitmapImage>> _bitmapCache = new(StringComparer.OrdinalIgnoreCase);
+
 	public object? Convert(object value, Type targetType, object parameter, string language)
 	{
 		if (value is not string fileName) return null;
 
-		var bitmapImage = new BitmapImage();
+		// In this case, we take the bet that the filename and the file will "always" be valid. As such, we pre-allocate a weak reference before knowing if the file will be read successfully.
+		BitmapImage? bitmapImage;
+		if (_bitmapCache.TryGetValue(fileName, out var wr))
+		{
+			if (wr.TryGetTarget(out bitmapImage)) return bitmapImage;
+			bitmapImage = new();
+			wr.SetTarget(bitmapImage);
+		}
+		else
+		{
+			bitmapImage = new();
+			_bitmapCache.Add(fileName, new(bitmapImage, false));
+		}
+
 		LoadImage(bitmapImage, fileName);
 		return bitmapImage;
 	}
