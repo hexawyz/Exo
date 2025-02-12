7d403c1f03db2588b13e0c90350fa6dc81be52e3
hexawyz <8518235+hexawyz@users.noreply.github.com>
2025-02-12 01:43:28 +0100

    ✨ Hack in a way for the inner components nested in a ScrollViewer to not be greedy.
    This does not handle resize to a smaller size properly (resize does not happen), but it does handle the rest.
    I hope there is a better way to do this, but it will do the job for now.

---
+++ src/Exo/Ui/Exo.Settings.Ui/Controls/NonGreedyContainer.cs
@@ -0,0 +1,53 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Windows.Foundation;
+
+namespace Exo.Settings.Ui.Controls;
+
+internal sealed class NonGreedyScrollViewer : ContentControl
+{
+	private Size? _finalSize;
+
+	public NonGreedyScrollViewer()
+	{
+		HorizontalContentAlignment = HorizontalAlignment.Stretch;
+		VerticalAlignment = VerticalAlignment.Stretch;
+		SizeChanged += OnSizeChanged;
+	}
+
+	private void OnSizeChanged(object sender, SizeChangedEventArgs e)
+	{
+		InvalidateMeasure();
+	}
+
+	protected override Size MeasureOverride(Size availableSize)
+	{
+		if (_finalSize is { } finalSize)
+		{
+			return base.MeasureOverride(_finalSize.GetValueOrDefault());
+		}
+		else if (float.IsPositiveInfinity(availableSize._height))
+		{
+			var maxSize = base.MeasureOverride(availableSize);
+			double minHeight = MinHeight;
+			var minSize = base.MeasureOverride(new(availableSize._width, double.IsNaN(minHeight) || minHeight <= 0 ? 1 : minHeight));
+			return new(maxSize._width, Math.Min(maxSize._height, minSize._height));
+		}
+		return base.MeasureOverride(availableSize);
+	}
+
+	protected override Size ArrangeOverride(Size finalSize)
+	{
+		if (_finalSize is null)
+		{
+			_finalSize = finalSize;
+			InvalidateMeasure();
+			return base.ArrangeOverride(finalSize);
+		}
+		else
+		{
+			_finalSize = null;
+			return base.ArrangeOverride(finalSize);
+		}
+	}
+}
