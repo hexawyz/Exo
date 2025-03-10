180aefdb910dc3dbf8e1873122a1cbdcdbec6c81
hexawyz <8518235+hexawyz@users.noreply.github.com>
2025-03-11 00:26:15 +0100

    ✏️ Add a few more NvAPI error codes.

--- src/Exo/Devices/Exo.Devices.NVidia/NvApiError.cs
+++ src/Exo/Devices/Exo.Devices.NVidia/NvApiError.cs
@@ -1,7 +1,23 @@
-﻿namespace Exo.Devices.NVidia;
+namespace Exo.Devices.NVidia;
 
 public enum NvApiError : int
 {
 	Success = 0,
+	Error = -1,
+	LibraryNotFound = -2,
+	NoImplementation = -3,
+	ApiNotInitialized = -4,
+	InvalidArgument = -5,
+	NvidiaDeviceNotFound = -6,
+	EndEnumeration = -7,
+	InvalidHandle = -8,
+	IncompatibleStructVersion = -9,
+	HandleInvalidated = -10,
+	OpenGlContextNotCurrent = -11,
+	NoGlExpert = -12,
+	InstrumentationDisabled = -13,
+	InvalidPointer = -14,
+	NoGlNsight = -15,
+	InvalidUserPrivilege = -137,
 	GpuNotPowered = -220,
 }
