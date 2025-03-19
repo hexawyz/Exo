c76e0287da048215347ea8714355008e245fcfef
hexawyz <8518235+hexawyz@users.noreply.github.com>
2025-03-19 21:05:57 +0100

    🐛 Add the missing SensorNotFoundException.

---
+++ src/Exo/Service/Exo.Service.Core/SensorNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿namespace Exo.Service;
+
+public class SensorNotFoundException : Exception
+{
+	public SensorNotFoundException() : this("The requested sensor was not found on the device.")
+	{
+	}
+
+	public SensorNotFoundException(string? message) : base(message)
+	{
+	}
+}
+
