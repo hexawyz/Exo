08946e8b22109579d05246fd87d60c976983cadf
hexawyz <8518235+hexawyz@users.noreply.github.com>
2025-04-13 21:20:33 +0200

    🐛 UI: Fix lighting effect padding computation.

--- src/Exo/Ui/Exo.Settings.Ui/ViewModels/LightingEffectViewModel.cs
+++ src/Exo/Ui/Exo.Settings.Ui/ViewModels/LightingEffectViewModel.cs
@@ -1,4 +1,5 @@
 using System.Collections.ObjectModel;
+using System.Numerics;
 using Exo.Contracts;
 
 namespace Exo.Settings.Ui.ViewModels;
@@ -26,38 +27,51 @@ internal sealed class LightingEffectViewModel
 		if (properties.IsDefaultOrEmpty) return ReadOnlyCollection<PropertyViewModel>.Empty;
 
 		bool isVariableLength = false;
-		var lengths = new uint[properties.Length];
+		var alignmentAndLengths = new (uint Align, uint Length)[properties.Length];
 		for (int i = 0; i < properties.Length; i++)
 		{
-			uint length = properties[i].DataType switch
+			var property = properties[i];
+			(uint align, uint length) = property.DataType switch
 			{
-				DataType.UInt8 or DataType.Int8 or DataType.ColorGrayscale8 => 1,
-				DataType.UInt16 or DataType.Int16 or DataType.Float16 or DataType.ColorGrayscale16 => 2,
-				DataType.UInt32 or DataType.UInt32 or DataType.Float32 or DataType.ColorRgbw32 or DataType.ColorArgb32 => 4,
-				DataType.UInt64 or DataType.Int64 or DataType.Float64 or DataType.TimeSpan or DataType.DateTime => 8,
-				DataType.Guid => 16,
-				_ => 0
+				DataType.UInt8 or DataType.Int8 or DataType.ColorGrayscale8 => (1u, 1u),
+				DataType.UInt16 or DataType.Int16 or DataType.Float16 or DataType.ColorGrayscale16 => (2u, 2u),
+				DataType.UInt32 or DataType.UInt32 or DataType.Float32 or DataType.ColorRgbw32 or DataType.ColorArgb32 => (4u, 4u),
+				DataType.UInt64 or DataType.Int64 or DataType.Float64 or DataType.TimeSpan or DataType.DateTime => (8u, 8u),
+				DataType.Guid => (8u, 16u),
+				DataType.ColorRgb24 => (1u, 3u),
+				DataType.ArrayOfColorGrayscale8 => (1u, (uint)(property.ArrayLength ?? 1)),
+				DataType.ArrayOfColorGrayscale16 => (2u, 2 * (uint)(property.ArrayLength ?? 1)),
+				DataType.ArrayOfColorRgb24 => (1u, 3 * (uint)(property.ArrayLength ?? 1)),
+				DataType.ArrayOfColorRgbw32 or DataType.ArrayOfColorArgb32 => (4u, 4 * (uint)(property.ArrayLength ?? 1)),
+				_ => (0u, 0u)
 			};
-			lengths[i] = length;
+			alignmentAndLengths[i] = (align, length);
 			if (length == 0) isVariableLength = true;
 		}
 
 		var vm = new PropertyViewModel[properties.Length];
 
 		uint offset = 0;
+		uint globalAlign = 0;
 		for (int i = 0; i < properties.Length; i++)
 		{
 			var property = properties[i];
-			uint length = lengths[i];
+			uint length = alignmentAndLengths[i].Length;
 			uint padding = 0;
 			if (!isVariableLength)
 			{
 				offset += length;
-				if ((uint)(i + 1) < (uint)lengths.Length)
+				if ((uint)(i + 1) < (uint)alignmentAndLengths.Length)
 				{
-					uint align = lengths[i + 1];
-					if (align == 16) align = 8;
+					uint align = alignmentAndLengths[i + 1].Align;
 					padding = (uint)-(int)offset & (align - 1);
+					offset += padding;
+					if (align > globalAlign) globalAlign = align;
+				}
+				else if (globalAlign > 1)
+				{
+					padding = (uint)-(int)offset & (globalAlign - 1);
+					offset += padding;
 				}
 			}
 			if (property.DataType is DataType.ArrayOfColorRgb24)
